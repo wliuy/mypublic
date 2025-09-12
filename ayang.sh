927275235808be2b20f8e99a73427904f59186d5
wliuy <51187046+wliuy@users.noreply.github.com>
2025-09-12 10:39:48 +0800

    Update ayang.sh

--- ayang.sh
+++ ayang.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 #
-# AYANG's Toolbox v2.1.3 (菜单项目对齐优化)
+# AYANG's Toolbox v2.1.4 (菜单项目对齐优化)
 #
 
 # --- 全局配置 ---
-readonly SCRIPT_VERSION="2.1.3"
+readonly SCRIPT_VERSION="2.1.4"
 readonly SCRIPT_URL="https://raw.githubusercontent.com/wliuy/mypublic/refs/heads/main/ayang.sh"
 
 # --- 颜色定义 ---
